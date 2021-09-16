bed8ffa56c6308c8cc23bdd7b0c3b4f527696aab
ZeraMalkav <razprog@hotmail.com>
2021-09-16 18:45:25 -0300

    Added the option for the user to enter how many items he's going to process.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,41 @@
 use std::io;
 use std::io::Write;
-
-const ARRAY_SIZE: usize = 3;
 const TAX_VALUE: f64 = 5.5;
 
 fn main() {
-    let mut array_of_items: [(u32, f32); ARRAY_SIZE] = [(0, 0.0); ARRAY_SIZE];
+    let mut vector_of_items: Vec<(u32, f32)> = Vec::new();
     let mut input_str = String::new();
 
     println!(
         "Welcome to the self-checkout system!.. Please provide the information requested below..."
     );
 
-    handle_input(&mut input_str, &mut array_of_items);
-    display_array(array_of_items);
-    let subtotal: f64 = process_subtotal(array_of_items);
+    handle_input(&mut input_str, &mut vector_of_items);
+    display_array(&vector_of_items);
+    let subtotal: f64 = process_subtotal(vector_of_items);
     let tax_total: f64 = calculate_tax(subtotal);
     let checkout_value = process_checkout(subtotal, tax_total);
 
     display_checkout(subtotal, tax_total, checkout_value);
 }
 
-fn handle_input(mut input_string: &mut String, array: &mut [(u32, f32); ARRAY_SIZE]) {
-    for (index, array_val) in array.iter_mut().enumerate() {
+fn handle_input(mut input_string: &mut String, vector: &mut Vec<(u32, f32)>) {
+    print!("How many items are you going to take in this purchase? ");
+    io::stdout().flush().unwrap();
+    input_string.clear();
+    io::stdin()
+        .read_line(&mut input_string)
+        .expect("Failed to read line");
+
+    let item_amount: usize = match input_string.trim().parse() {
+        Ok(num) => num,
+        Err(e) => {
+            println!("{}", e);
+            3
+        }
+    };
+
+    for index in 0..item_amount {
         print!(
             "Please input how many of Item {} you're getting: ",
             index + 1
@@ -60,15 +73,14 @@ fn handle_input(mut input_string: &mut String, array: &mut [(u32, f32); ARRAY_SI
             }
         };
 
-        array_val.0 = item_amount;
-        array_val.1 = item_price;
+        vector.push((item_amount, item_price));
     }
 }
 
-fn process_subtotal(array: [(u32, f32); ARRAY_SIZE]) -> f64 {
+fn process_subtotal(vector: Vec<(u32, f32)>) -> f64 {
     let mut subtotal: f64 = 0.0;
 
-    subtotal += array
+    subtotal += vector
         .iter()
         .map(|(a, b)| f64::from(*a as f32 * b))
         .sum::<f64>();
@@ -84,8 +96,8 @@ fn process_checkout(subtotal: f64, tax_total: f64) -> f64 {
     return subtotal + tax_total;
 }
 
-fn display_array(array: [(u32, f32); ARRAY_SIZE]) {
-    for (index, elem) in array.iter().enumerate() {
+fn display_array(vector: &Vec<(u32, f32)>) {
+    for (index, elem) in vector.iter().enumerate() {
         println!(
             "Item {} costs $ {:.2}, you're taking {} units ===> Total of: $ {:.2}",
             index + 1,
